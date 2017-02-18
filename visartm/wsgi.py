b23151c7ff627c37bfd8a40e0b335099978ab0f8
Dmytry Fedoyaka <fedimser@yandex.ru>
2017-02-19 00:20:32 +0300

    gunicorn

--- visartm/wsgi.py
+++ visartm/wsgi.py
@@ -1,5 +1,5 @@
 """
-WSGI config for artmonline project.
+WSGI config for visartm project.
 
 It exposes the WSGI callable as a module-level variable named ``application``.
 
@@ -11,7 +11,7 @@ import os
 
 from django.core.wsgi import get_wsgi_application
 
-os.environ.setdefault("DJANGO_SETTINGS_MODULE", "artmonline.settings")
+os.environ.setdefault("DJANGO_SETTINGS_MODULE", "visartm.settings")
 
 application = get_wsgi_application()
 
