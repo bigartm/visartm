6067b912ebfac2cd42ef778f01ce9f663657928c
Dmytry Fedoyaka <fedimser@yandex.ru>
2017-02-19 10:57:35 +0300

    prc fix

--- accounts/urls.py
+++ accounts/urls.py
@@ -8,7 +8,7 @@ urlpatterns = [
 	url(r'^signup', accounts_views.signup),
 	url(r'^sendmail', accounts_views.sendmail),
 	url(r'password_reset_done$', auth_views.password_reset_done, name='password_reset_done'),
-	url(r'password_reset_confirm/(?P<uidb64>[0-9A-Za-z]+)-(?P<token>.+)/', auth_views.password_reset_confirm, name='5password_reset_confirm'),
+	url(r'password_reset_confirm/(?P<uidb64>[0-9A-Za-z]+)-(?P<token>.+)/', auth_views.password_reset_confirm, name='password_reset_confirm'),
 	url(r'password_reset_complete$', auth_views.password_reset_complete, name='password_reset_complete'),	
 	url(r'password_reset', auth_views.password_reset, name='password_reset'),	
 	url(r'^user/(?P<user_name>\w+)$', accounts_views.account_view)
