8b01c1bf2bea44b875da86d504ba8d215bdb46fc
Dmytry Fedoyaka <fedimser@yandex.ru>
2017-04-04 19:19:09 +0200

    f

--- algo/clib/arrange.c
+++ algo/clib/arrange.c
@@ -3,7 +3,7 @@
 void* arrange(int n, double* x, int* answer)
 {
     double sum = 0;
-    int i;
+    int i,j;
     for (i=0;i<n;++i) {
         for(j=0;j<n;j++)
         {
@@ -13,4 +13,8 @@ void* arrange(int n, double* x, int* answer)
     for(i=0;i<n;++i) answer[i] = i;
     answer[0] = 2;
     answer[2] = 0;
+}
+
+int main() {
+    return 0;
 }
