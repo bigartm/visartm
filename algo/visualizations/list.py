24b3b07917429f8411afe14eb5d2e46394c761f6
Dmytro Fedoriaka <fedimser@yandex.ru>
2017-06-12 11:53:45 +0300

    list visualization

---
+++ algo/visualizations/list.py
@@ -0,0 +1,5 @@
+from models.models import Topic, TopicInTopic 
+
+def visual(vis, params):
+	topics = Topic.objects.filter(model=vis.model, layer=vis.model.layers_count).order_by("spectrum_index")
+	return "<br>".join([topic.top_words_list(count=15) for topic in topics])
