a8bcd7b3f54d49415a6f5044b85c34caba72e6c2
Dmytro Fedoriaka <fedimser@yandex.ru>
2017-06-20 23:49:46 +0300

    fix

--- datasets/management/commands/collectdocs.py
+++ datasets/management/commands/collectdocs.py
@@ -9,6 +9,10 @@ class Command(BaseCommand):
 		input_path = os.path.join(settings.BASE_DIR, "templates", "docs")
 		output_path = os.path.join(settings.BASE_DIR, "static", "docs")
 		for file_name in os.listdir(input_path):
-			with open(os.path.join(output_path, file_name), "w", encoding="utf-8") as f:
-				f.write(render_to_string('docs/%s' % file_name))
-			#print("Done: %s" % file_name)
+			in_file = os.path.join(input_path, file_name)
+			out_file = os.path.join(output_path, file_name)
+			
+			if not os.path.exists(out_file) or (os.path.getmtime(in_file) > os.path.getmtime(out_file)):
+				with open(os.path.join(output_path, file_name), "w", encoding="utf-8") as f:
+					f.write(render_to_string('docs/%s' % file_name))
+				print("Collected: %s" % file_name)
